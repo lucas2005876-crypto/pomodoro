615b9bb3697e86b22eb11f8e88f527c80017c944
lucas <lucas2005876@gmail.com>
2026-01-27 14:16:20 -0300

    feito o temporizador

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const card = document.querySelector('.app__card')
-const starterPause = document.querySelector('.app__card-primary-button')
+const starterPause = document.getElementById('start-pause')
 const img = document.querySelector(".app__image")
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const musicaInput = document.getElementById('alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const playAudio = new Audio('sons/play.wav')
+const pauseA = new Audio('sons/pause.mp3')
+const alerta = new Audio ('sons/beep.mp3')
+const timerArea = document.getElementById('timer')
+let tempoDecorrido = 1500
+let intervalo = null
 
 const foco = 1500
 const descansoCurto = 300
 const descansoLongo = 900
 
+
 musica.loop = true
+
 musicaInput.addEventListener('change', () => {
     if(musica.paused){
         musica.play()
@@ -25,21 +33,25 @@ musicaInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () =>{
+    tempoDecorrido = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorrido = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorrido = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto){
+    mostrarTempo()
     botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
@@ -65,3 +77,49 @@ function alterarContexto(contexto){
     }
 }
 
+const contagemRegressiva = () => {
+    if(tempoDecorrido <=0){ 
+        alerta.play()
+        zerar()
+        return
+
+    }
+    tempoDecorrido -= 1
+    mostrarTempo()
+}
+
+
+starterPause.addEventListener('click', iniciarPausar)
+
+
+function iniciarPausar() {
+    if(intervalo){
+        zerar()
+        pauseA.play()
+        starterPause.textContent = "Começar"
+        return
+    }
+    intervalo = setInterval(contagemRegressiva, 1000)
+    playAudio.play()
+    starterPause.textContent = "Pausar"
+}
+
+function zerar(){
+    clearInterval(intervalo)
+    intervalo = null
+    tempoDecorrido = 1500
+}
+
+function mostrarTempo() {
+    const minutos = Math.floor(tempoDecorrido / 60)
+    const segundos = tempoDecorrido % 60
+
+    const minutosFormatados = String(minutos).padStart(2, '0')
+    const segundosFormatados = String(segundos).padStart(2, '0')
+
+    timerArea.innerHTML = `${minutosFormatados}:${segundosFormatados}`
+
+
+}
+
+mostrarTempo()
