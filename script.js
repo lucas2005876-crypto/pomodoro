055e97016f659e11fec9c27080671d486a94ae7e
lucas <lucas2005876@gmail.com>
2026-01-28 14:35:26 -0300

    commit extra para teste

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//codigo atualizado
+
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
