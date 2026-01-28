0cf0a895f451e81635b5eadec690c783a2848e4f
lucas <lucas2005876@gmail.com>
2026-01-28 13:59:28 -0300

    sessao de atividades completa

--- script-crud.js
+++ script-crud.js
@@ -1,34 +1,68 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formulario = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
-const listaTarefas = []
+const areaTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+const listaTarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
- function criarElementoTarefa(tarefa){
-    const li = document.createElement('li')
-    li.classList.add('app__section-task-list-item')
+function atualizarTarefas (){
+    localStorage.setItem('tarefas', JSON.stringify(listaTarefas))
+}
+function criarElementoTarefa(tarefa) {
+    const itemLista = document.createElement('li')
+    itemLista.classList.add('app__section-task-list-item')
 
-    const svg = document.createElement('svg')
+    const svg = document.createElement('div')
     svg.innerHTML = `
-        <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
-    const paragrafo = document.createElement('p')
-    paragrafo.textContent = tarefa.descricao
-    paragrafo.classList.add('app__section-task-list-item-description')
 
-    const botao = document.createElement('button')
-    const imgBotao = document.createElement('img')
-    imgBotao.setAttribute('src', '/imagens/edit.png')
+    const paragrafoDescricao = document.createElement('p')
+    paragrafoDescricao.textContent = tarefa.descricao
+    paragrafoDescricao.classList.add('app__section-task-list-item-description')
 
-    botao.append(imgBotao)
-    li.append(svg)
-    li.append(paragrafo)
-    li.append(botao)
+    const botaoEditar = document.createElement('button')
+    const imagemBotao = document.createElement('img')
+    botaoEditar.classList.add('app-button-edit')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
+
+    botaoEditar.onclick = (evento) => {
+        evento.stopPropagation()
+
+        const novaDescricao = prompt('Qual é o novo nome da tarefa?')
+        if (novaDescricao) {
+            tarefa.descricao = novaDescricao
+            paragrafoDescricao.textContent = novaDescricao
+            atualizarTarefas()
+        }
+    }
+
+    botaoEditar.append(imagemBotao)
+
+    itemLista.append(svg)
+    itemLista.append(paragrafoDescricao)
+    itemLista.append(botaoEditar)
+
+    itemLista.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+
+        const todasAsTarefas = document.querySelectorAll(
+            '.app__section-task-list-item'
+        )
+
+        todasAsTarefas.forEach(item => {
+            item.classList.remove('app__section-task-list-item-active')
+        })
+
+        itemLista.classList.add('app__section-task-list-item-active')
+    }
+
+    return itemLista
 }
+
 
 btnAdicionarTarefa.addEventListener('click', () => {
 
@@ -41,6 +75,15 @@ formulario.addEventListener('submit', (evento) => {
         descricao : textArea.value
     }
     listaTarefas.push(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(listaTarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    areaTarefas.append(elementoTarefa)
+    atualizarTarefas()
+    textArea.value = ''
+    formulario.classList.add('hidden')
 })
+
+listaTarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    areaTarefas.append(elementoTarefa)
+});
 
